1ec7fe0247dd0da44050ea86cf328e21e609a97a
mingchengzack <chengmingjtt123@gmail.com>
2020-01-19 16:06:18 -0800

    added front end

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,15 @@
 import React from "react";
 import "./App.css";
+import { Container } from "semantic-ui-react";
+import ToDoList from "./components/ToDoList";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div>
+      <Container>
+        <ToDoList />
+      </Container>
+    </div>
+  );
 }
-
 export default App;
